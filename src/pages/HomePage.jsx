d967a4017531c78e4279a9acee0307a1e99f5d83
mamun060 <obujraju420@gmail.com>
2021-12-14 13:36:24 +0600

    footer section design done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import Slider from "../components/Slider";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
@@ -15,6 +16,7 @@ const HomePage = () => {
       <Categories />
       <Products />
       <Newsletter />
+      <Footer />
     </div>
   );
 };
