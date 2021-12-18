b869636e40118197b5a56f26039ecc14631aeb1d
mamun060 <obujraju420@gmail.com>
2021-12-18 17:22:47 +0600

    complete product details page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Fragment } from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
 import ProductListPage from './pages/ProductListPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
 
 function App() {
   return (
     <div className="App">
       <Fragment>
          {/* <HomePage /> */}
-         <ProductListPage />
+         {/* <ProductListPage /> */}
+         <ProductDetailsPage />
       </Fragment>
     </div>
   );
